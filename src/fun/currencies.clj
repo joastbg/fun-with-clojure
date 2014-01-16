13a9e1185903e9c1c66419b4b7c3961ad7c2c9d2
Johan Astborg <johan@gecemmo.com>
2014-01-16 17:38:51 +0100

    Cleanup and bugfix

--- src/fun/currencies.clj
+++ src/fun/currencies.clj
@@ -10,29 +10,43 @@
 (def USDCHF (atom 0.9026))
 (def IDENDITY (atom 1.0))
 
-(def rr {:USD {:EUR nil
-               :JPY USDJPY
-               :CHF USDCHF
-               :GBP nil}
+;; Supported currencies
+
+(def rr {:USD {:JPY USDJPY
+               :CHF USDCHF}
          :EUR {:USD EURUSD}
-         :CHF {:USD nil}
-         :JPY {:USD nil}
          :GBP {:USD GBPUSD}})
 
+;; Helpers
+
 (defn convert [from to]
-  (assoc from :oldamount (:amount from)
+  (assoc from :old from
          :amount ((:converter (meta from)) from to)
          :currency to))
 
+;; Round to arbitrary precision
+(defn round [v d]
+  (let [f (Math/pow 10 (- d 1))]
+   (double (/ (Math/round (* v f)) f))))
+
+(defn round [v d]
+  (/ (Math/round (* v (Math/pow 10 d))) (double (Math/pow 10 d))))
+
+(defn roundq [v]
+  (round v 4))
+
+(defn rounda [v]
+  (round v 2))
+
 (defn cash [n c]
   (let [converter
-        (fn [c d]
-          ;; Fallback if error occurs, no conversion
-          (let [x (d ((:currency c) rr))
-                y ((:currency c) (d rr))
-                z (if (nil? y) IDENDITY y)
-                rate (if (nil? x) (-> z deref /) (-> x deref))]
-            ;(do (println "DEBUG :: " rate " :: " n " :: " c)
+        (fn [c t]
+          (let [f (:currency c)
+                x (t (f rr))
+                y (f (t rr))
+                rate (if (nil? x)
+                       (if (nil? y) 1.0  (-> y deref /))
+                       (if-not (nil? x) (-> x deref)))]
           (* (:amount c) rate)))]
    (with-meta {:amount n :currency c} {:converter converter})))
 
@@ -62,21 +76,6 @@
   (round v 2))
 
 ;; ----------------------------------------------------------------------
-;; Sample dynamic usage (background thread to simulate rates)
-;; ----------------------------------------------------------------------
-
-;; Generate random quite in interval
-(defn randi [l u]
-  (+ 0.10 l (* (- u l) (rand))))
-
-;; Random periodic call to function fun
-(defn periodicly [fun tf]
-  "starts a thread that calls function every time ms"
-  (let [thread (new Thread
-     (fn [] (loop [] (fun) (Thread/sleep (* (rand) tf)) (recur))))]
-        (.start thread) thread))
-
-;; ----------------------------------------------------------------------
 ;; Test cases
 ;; ----------------------------------------------------------------------
 (defn test-convert [c d]
@@ -99,6 +98,22 @@
     (assert (test-convert :USD :CHF))
     (assert (test-convert :CHF :USD))))
 
+;; ----------------------------------------------------------------------
+;; Sample dynamic usage (background thread to simulate rates)
+;; ----------------------------------------------------------------------
+
+;; Generate random quote in interval
+(defn randi [l u]
+  (+ 0.10 l (* (- u l) (rand))))
+
+;; Random periodic call to function fun
+(defn periodicly [fun tf]
+  "starts a thread that calls function every time ms"
+  (let [thread (new Thread
+     (fn [] (loop [] (fun) (Thread/sleep (* (rand) tf)) (recur))))]
+        (.start thread) thread))
+
+;; Update rates
 (defn update-rates []
   (do
     (reset! EURUSD (randi 1.10 1.40))
@@ -107,27 +122,38 @@
     (println "New rate for USD/JPY" @USDJPY)
     ))
 
+;; Applies the function f to key k in map m
 (defn mapdoto [m k f]
   (assoc m k (-> (k m) f)))
 
+;; Converts and formatts the cash object
 (defn print-convert [cash to]
   (let [fmt (fn [kwd] (apply str (rest (str kwd))))]
-    (do (let [m (convert cash1 to)]
+    (do (let [m (convert cash to)]
       (println (str ":: "(fmt (:currency cash)) "->" (fmt to) " :: "
         (into (sorted-map) (assoc (mapdoto m :amount rounda)
-          :rate (roundq (/ (:amount m) (:oldamount m)))))))))))
+          :rate (roundq (/ (:amount m) (:amount (:old m))))))))))))
 
+;; Convert some cash
 (defn get-and-convert []
   (do
     (print-convert cash1 :JPY)
+    (print-convert cash1 :EUR)
     (print-convert cash2 :USD)
+    (print-convert cash1 :CHF)
+    (print-convert cash1 :GBP)
     (print-convert cash3 :USD)))
 
 (defn -main [& args]
   (do
-    (println "-- Testing a Currency implementation in Clojure\n")
+    (println "-------------------------------------------------")
+    (println "   Testing a Currency implementation in Clojure\n")
+    (println "   (C) Johan Astborg, 2013\n   joastbg@gmail.com\n")
+
     (println "   1. Running test cases")
     (run-test-cases)
-    (println "   2. Starting simulation thread (separate threads for update and conversion)\n")
-    (periodicly update-rates (randi 1000 1900))
+
+    (println "   2. Starting simulation thread")
+    (println "-------------------------------------------------\n")
+    (periodicly update-rates (randi 50 900))
     (periodicly get-and-convert 1000)))
