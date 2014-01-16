c53e2a60ca35c94536652b89ffec085117f1b49a
Johan Astborg <joastbg@gmail.com>
2014-01-16 12:34:38 +0100

    Handle updates of atom

--- currencies.clj
+++ currencies.clj
@@ -1,5 +1,5 @@
 ;; ----------------------------------------------------------------------
-;; Experimental implementation of currencies 
+;; Experimental implementation of currencies
 ;; ----------------------------------------------------------------------
 
 (def EURUSD (atom 1.3668))
@@ -7,24 +7,41 @@
 (def GBPUSD (atom 1.6484))
 (def USDCHF (atom 0.9026))
 
+(deref EURUSD)
+
+(def t1 *)
+(def t1 /)
+
 ;; TODO: Handle updates using lambda
-(def rr {:USD {:EUR (/ 1 @EURUSD)
-               :JPY @USDJPY 
-               :CHF @USDCHF 
-               :GBP (/ 1 @GBPUSD)} 
-         :EUR {:USD @EURUSD}
-         :CHF {:USD (/ 1 @USDCHF)}
-         :JPY {:USD (/ 1 @USDJPY)}
-         :GBP {:USD @GBPUSD}})
+(def rr {:USD {:EUR [EURUSD /]
+               :JPY [USDJPY *]
+               :CHF [USDCHF *]
+               :GBP [GBPUSD /]}
+         :EUR {:USD [EURUSD *]}
+         :CHF {:USD [USDCHF /]}
+         :JPY {:USD [USDJPY /]}
+         :GBP {:USD [GBPUSD *]}})
+
+
+(first (:EUR (:USD rr)))
+(:EUR (:USD rr))
+(last '(EURUSD /))
+;(def test (with-meta {:EUR EURUSD} {:modifier t1})) ((:modifier (meta
+;test)) (:EUR test))
+(-> EURUSD deref /)
+
+(-> (first (:EUR (:USD rr))) deref ((last (:EUR (:USD rr)))))
+(-> (first (:USD (:EUR rr))) deref ((last (:USD (:EUR rr)))))
 
 (defn convert [c d]
   (assoc c :amount ((:converter (meta c)) c d) :currency d))
 
-
 (defn cash [n c]
-  (let [converter (fn [c d] (* (:amount c) (d ((:currency c) rr))))]
+  (let [converter
+        (fn [c d]
+          (* (:amount c)
+             (-> (first (:USD (:EUR rr))) deref ((last (:USD (:EUR rr)))))))]
    (with-meta {:amount n :currency c} {:converter converter})))
-
 
 ;; ----------------------------------------------------------------------
 ;; Sample static usage
@@ -46,13 +63,13 @@
 (convert cash1 :EUR)
 
 ;; ----------------------------------------------------------------------
-;; Test cases 
+;; Test cases
 ;; ----------------------------------------------------------------------
 (defn test-convert [c d]
   (let [fcash (cash 1000.0 c)
         acash (convert (convert fcash d) c)]
     (and (= (:currency acash (:currency fcash)))
-         (let [round (fn [a] 
+         (let [round (fn [a]
                        (double (/ (Math/round (* a 100)) 100)))
                a1 (round (:amount fcash))
                a2 (round (:amount acash))]
